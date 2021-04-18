20aa8b40f95fbec0a8da2c05647b42e54cbe8bc2
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2021-04-18 23:20:46 +0530

    added nav bar js adjusted size of pages

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+//nav buttons
+
+//smooth scrolling
+document.querySelector('.nav_links').addEventListener('click',function(e){
+    e.preventDefault();
+    
+    if(e.target.classList.contains('nav_link')){
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior:"smooth"});
+    }
+});
+
+//hover effect
+const nav=document.querySelector(".nav");
+const handleHover =function(e){
+    if(e.target.classList.contains('nav_link')){
+        const link=e.target;
+        const siblings=link.closest('.nav').querySelectorAll('.nav_link');
+        const logo=link.closest('.nav').querySelector('img');
+        siblings.forEach(el=>{
+            if(el!==link){
+                el.style.opacity=this;
+            } 
+        });
+        logo.style.opacity=this;
+    };
+}
+nav.addEventListener('mouseover',handleHover.bind(0.6));
+nav.addEventListener('mouseout',handleHover.bind(1));
